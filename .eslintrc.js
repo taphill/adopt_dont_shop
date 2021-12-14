35ad1e9728d19f2840b1a05bf09e42e98076530b
Taylor Phillips <taylorscottphillips@gmail.com>
2021-12-13 22:55:47 -0600

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 13,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
